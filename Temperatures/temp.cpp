3a0cdf0963fdea28b938e7ec3f05737a32fa4db8
vlad-gor <gorunov341@yandex.ru>
2020-11-17 23:51:35 +0300

    temperature done

--- Temperatures/temp.cpp
+++ Temperatures/temp.cpp
@@ -2,17 +2,47 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <utility>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    int n; // the number of temperatures to analyse
-    cin >> n; cin.ignore();
-    for (int i = 0; i < n; i++) {
-        int t; // a temperature expressed as an integer ranging from -273 to 5526
-        cin >> t; cin.ignore();
+    int n;
+    std::vector<int> values(n);
+    cin >> n;
+    cin.ignore();
+
+    for (int i = 0; i < n; i++)
+    {
+        int t;
+        cin >> t;
+        cin.ignore();
+        values.push_back(t);
     }
 
-    cout << "result" << endl;
+    int min_t = 5527;
+    for (int i = 0; i < n; i++)
+    {
+        if (abs(values[i]) < abs(min_t))
+        {
+            min_t = values[i];
+        }
+        else if (abs(values[i]) == abs(min_t))
+        {
+            if (values[i] > min_t)
+            {
+                min_t = values[i];
+            }
+        }
+    }
+
+    if(n){
+        std::cout << min_t << std::endl;
+    }
+    else{
+        std::cout << 0 << std::endl;
+    }
+    return 0;
 }
