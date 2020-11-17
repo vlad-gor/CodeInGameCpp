c56c42a1919cfa8671f4dbb2b70e1d2965c01334
vlad-gor <gorunov341@yandex.ru>
2020-11-18 00:01:26 +0300

    sales begin

---
+++ TravellingSalesman/salesman.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+    int N;
+    cin >> N; cin.ignore();
+    for (int i = 0; i < N; i++) {
+        int X;
+        int Y;
+        cin >> X >> Y; cin.ignore();
+    }
+
+    // Write an answer using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    cout << "distance" << endl;
+}
